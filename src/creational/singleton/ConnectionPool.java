2cdf16938c581a53fbf50169e87f632db34364db
manoharaurala <manoharaurala@gmail.com>
2024-10-27 18:23:07 +0530

    Version-2->Final Design of Singleton class

--- src/creational/singleton/ConnectionPool.java
+++ src/creational/singleton/ConnectionPool.java
@@ -1,16 +1,28 @@
 package creational.singleton;
 
 public class ConnectionPool {
-    private static  ConnectionPool connectionPool=null;
+    // Step 3
+    private static ConnectionPool INSTANCE = null;// Eager initialization - slow startup
 
     private ConnectionPool() {
         // Step 1 - private constructor
     }
 
-    public static ConnectionPool getInstance(){
-        if(connectionPool==null){
-            connectionPool=new ConnectionPool();
+    public static ConnectionPool getInstance() {
+        // Step 2 - public static method
+        if (INSTANCE == null) {
+            // Step 4
+            synchronized (ConnectionPool.class) {
+                if (INSTANCE == null)
+                    INSTANCE = new ConnectionPool();
+            }
         }
-        return connectionPool;
+        return INSTANCE;
     }
 }
+
+// Cons of this approach:
+// 1. SRP violation
+// 2. Thread safety is not guaranteed
+// 3. Performance is not guaranteed in thread-safe mode - Double checked locking
+// Enum implementation, inner static class
--- src/creational/singleton/ConnectionPoolTest.java
+++ src/creational/singleton/ConnectionPoolTest.java
@@ -2,20 +2,14 @@ package creational.singleton;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
 
 
 public class ConnectionPoolTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
     public void getInstanceTest() {
-        ConnectionPool connectionPool=ConnectionPool.getInstance();
-        ConnectionPool connectionPool1=ConnectionPool.getInstance();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        ConnectionPool connectionPool1 = ConnectionPool.getInstance();
         Assertions.assertSame(connectionPool1, connectionPool, "If a new instance is created, it should be the same as the older one");
     }
 
